92704e104b0b477f90668846a37a0f80f81fb71e
Guillaume Rivas <GiyommStarsky@gmail.com>
2016-04-27 16:00:01 +0200

    Add custom CalendarView

--- app/src/main/java/com/agenda/ter/smartgenda/CalendarActivity.java
+++ app/src/main/java/com/agenda/ter/smartgenda/CalendarActivity.java
@@ -1,28 +1,19 @@
 package com.agenda.ter.smartgenda;
 
+import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CalendarView;
 
-import com.agenda.ter.database.SmartgendaDbHelper;
-
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashSet;
 
 public class CalendarActivity extends AppCompatActivity {
 
     public final static String EXTRA_SELECTED_DATE = "com.agenda.ter.smartgenda.SELECTED_DATE";
-
-    private CalendarView calendar;
-    private Date selectedDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,54 +21,24 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         //SQLiteDatabase database = openOrCreateDatabase(SmartgendaDbHelper.DATABASE_NAME,MODE_PRIVATE,null);
 
-        //Init the calendar view
-        calendar = (CalendarView) findViewById(R.id.calendar_view_id);
+        HashSet<Date> events = new HashSet<>();
 
-        //Select the current selected date to the current date
-        selectedDate = new Date(calendar.getDate());
+        com.agenda.ter.smartgenda.CalendarView cv = ((com.agenda.ter.smartgenda.CalendarView)findViewById(R.id.calendar_view));
+        cv.updateCalendar(events);
 
-        //Set the selected date as the calendar minimum date to display
-        calendar.setMinDate(selectedDate.getTime());
-
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        // assign event handler
+        cv.setEventHandler(new com.agenda.ter.smartgenda.CalendarView.EventHandler()
+        {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Log.d("DATE SELECTED",""+dayOfMonth+"/"+(month+1)+"/"+year);
-                String str_date = dayOfMonth+"/"+(month+1)+"/"+year;
-                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-                try {
-                    selectedDate = sdf.parse(str_date);
-                } catch (ParseException ex) {
-                    ex.printStackTrace();
-                }
+            public void onDayLongPress(Date date)
+            {
+                //show day dialog
+                showDayDialog(date);
+            }
+        });
 
-                AlertDialog.Builder eventDialogBuilder = new AlertDialog.Builder(CalendarActivity.this);
-                eventDialogBuilder.setTitle(str_date);
-                eventDialogBuilder.setMessage("Create, delete or modify an Event");
-
-                eventDialogBuilder.setNegativeButton(
-                        "Cancel",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                eventDialogBuilder.setPositiveButton(
-                        "Add Event",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Intent intent = new Intent(CalendarActivity.this, EventActivity.class);
-                                intent.putExtra(EXTRA_SELECTED_DATE,selectedDate.getTime());
-                                startActivity(intent);
-                            }
-                        });
-
-                eventDialogBuilder.show();
-
-                /*final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
+        /*
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(
                 CalendarActivity.this,android.R.layout.select_dialog_singlechoice);
                 arrayAdapter.add("Hardik");
                 arrayAdapter.add("Archit");
@@ -109,8 +70,36 @@ public class CalendarActivity extends AppCompatActivity {
                             }
                         });*/
 
-            }
-        });
+            //}
+        //});
+    }
+
+    private void showDayDialog(final Date selectedDate) {
+        AlertDialog.Builder eventDialogBuilder = new AlertDialog.Builder(CalendarActivity.this);
+        eventDialogBuilder.setTitle(new SimpleDateFormat("dd/MM/yyyy").format(selectedDate));
+        eventDialogBuilder.setMessage("Créez, modifiez ou supprimez une tâche.");
+
+        eventDialogBuilder.setNegativeButton(
+                "Retour",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        eventDialogBuilder.setPositiveButton(
+                "Ajoutez une tâche",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(CalendarActivity.this, EventActivity.class);
+                        intent.putExtra(EXTRA_SELECTED_DATE,selectedDate.getTime());
+                        startActivity(intent);
+                    }
+                });
+
+        eventDialogBuilder.show();
     }
 
     public void goToEventActivity(View view) {
