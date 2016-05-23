afffafc1d0ffe7f6c6c18c9bc111a7b02d7aedfb
yassineamin <yassine.amin@outlook.com>
2016-05-23 13:44:15 +0200

    Javadoc (EventContract et LocationContract)

--- app/src/main/java/com/agenda/ter/database/EventContract.java
+++ app/src/main/java/com/agenda/ter/database/EventContract.java
@@ -5,12 +5,28 @@ import android.provider.BaseColumns;
 /**
  * Created by Giyomm on 26/04/2016.
  */
+
+/**
+ * Represente la table Evenement dans la base de données
+ */
 public final class EventContract {
 
     public EventContract() {}
 
     /* Inner class that defines the table contents */
+
+
     public static abstract class EventEntry implements BaseColumns {
+        /**
+         * TABLE_NAME                       Le nom de la table
+         * COLUMN_NAME_EVENT_ID             L'identifiant de l'événement
+         * COLUMN_NAME_EVENT_NAME           Le nom de l'événement
+         * COLUMN_NAME_EVENT_DATE           La date de l'événement
+         * COLUMN_NAME_EVENT_TIME           L'heure de l'événement
+         * COLUMN_NAME_EVENT_DESCRIPTION    La description de l'événement
+         * COLUMN_NAME_EVENT_LOCATION_ID    L'identifiant du lieu de l'événement
+         * COLUMN_NAME_EVENT_EMAIL          L'email pour les rappels
+         */
         public static final String TABLE_NAME = "event";
         public static final String COLUMN_NAME_EVENT_ID = "event_id";
         public static final String COLUMN_NAME_EVENT_NAME = "event_name";
@@ -21,10 +37,18 @@ public final class EventContract {
         public static final String COLUMN_NAME_EVENT_NOTIFICATION_ID = "event_notification_id";
         public static final String COLUMN_NAME_EVENT_EMAIL = "event_email";
 
+        /**
+         * VARCHAR_TYPE         Represente le string varchar
+         * INTEGER_TYPE         Represente le string integer
+         * COMMA_SEP            Represente une virgule
+         */
         private static final String VARCHAR_TYPE = " VARCHAR";
         private static final String INTEGER_TYPE = " INTEGER";
         private static final String COMMA_SEP = ",";
 
+        /**
+         * Un champ qui contient une requete pour la creation de la table événement
+         */
         private static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE IF NOT EXISTS " + EventEntry.TABLE_NAME +
                         " (" +
@@ -42,6 +66,9 @@ public final class EventContract {
                                 ") REFERENCES "+ NotificationContract.NotificationEntry.TABLE_NAME + "("+ NotificationContract.NotificationEntry.COLUMN_NAME_NOTIFICATION_ID +")"+
                         " )";
 
+        /**
+         * Un champ contennant une requete pour la suppresion de la table d'un événement
+         */
         private static final String SQL_DELETE_ENTRIES =
                 "DROP TABLE IF EXISTS " + EventEntry.TABLE_NAME;
 
--- app/src/main/java/com/agenda/ter/database/LocationContract.java
+++ app/src/main/java/com/agenda/ter/database/LocationContract.java
@@ -5,12 +5,25 @@ import android.provider.BaseColumns;
 /**
  * Created by Giyomm on 26/04/2016.
  */
+
+/**
+ * Represente la table d'un lieu dans la base de données
+ */
 public final class LocationContract {
 
     public LocationContract() {}
 
     /* Inner class that defines the table contents */
     public static abstract class LocationEntry implements BaseColumns {
+        /**
+         * TABLE_NAME                           Le nom de la table
+         * COLUMN_NAME_LOCATION_ID              L'identifiant d'un lieu
+         * COLUMN_NAME_LOCATION_NAME            Le nom d'un lieu
+         * COLUMN_NAME_METEO_TEMPERATURE        La météo d'un lieu
+         * COLUMN_NAME_LOCATION_LONGITUDE       La longitude d'un lieu
+         * COLUMN_NAME_LOCATION_LATITUDE        La latitude d'un lieu
+         * COLUMN_NAME_METEO_ICON               L'icone d'un lieu
+         */
         public static final String TABLE_NAME = "location";
         public static final String COLUMN_NAME_LOCATION_ID = "location_id";
         public static final String COLUMN_NAME_LOCATION_NAME = "location_name";
@@ -19,11 +32,19 @@ public final class LocationContract {
         public static final String COLUMN_NAME_LOCATION_LATITUDE = "location_latitude";
         public static final String COLUMN_NAME_METEO_ICON = "meteo_icon";
 
+        /**
+         * VARCHAR_TYPE         Represente le string varchar
+         * INTEGER_TYPE         Represente le string integer
+         * COMMA_SEP            Represente une virgule
+         */
         private static final String VARCHAR_TYPE = " VARCHAR";
         private static final String INTEGER_TYPE = " INTEGER";
         private static final String REAL_TYPE = " REAL";
         private static final String COMMA_SEP = ",";
 
+        /**
+         * Un champ qui contient une requete pour la creation de la table location
+         */
         private static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE IF NOT EXISTS " + LocationEntry.TABLE_NAME +
                         " (" +
@@ -35,6 +56,9 @@ public final class LocationContract {
                         LocationEntry.COLUMN_NAME_METEO_ICON + VARCHAR_TYPE + "(255)" +
                         " )";
 
+        /**
+         * Un champ qui contient une requete pour la suppression de la table location
+         */
         private static final String SQL_DELETE_ENTRIES =
                 "DROP TABLE IF EXISTS " + LocationEntry.TABLE_NAME;
 
